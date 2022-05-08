c7bc6fddee12157bf5d4b319f9e5be8c99d74d3a
Andry Pebrianto <andrypeb227@gmail.com>
2022-05-08 23:50:49 +0700

    Uninstall eslint

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,26 +1,17 @@
 import { combineReducers } from "redux";
-<<<<<<< HEAD
 import listProductReducer from "./listProduct";
 import listAirlineReducer from "./listAirline";
 import detailProductReducer from "./detailProduct";
 import detailUserReducer from "./detailUser";
+import destinationReducer from './destination';
 
 const rootReducers = combineReducers({
   listProduct: listProductReducer,
   listAirline: listAirlineReducer,
   detailProduct: detailProductReducer,
   detailUser: detailUserReducer,
-});
-=======
-import userReducer from "./user";
-import detailUserReducer from "./detailUser";
-import destinationReducer from './destination';
-
-const rootReducers = combineReducers({
-  user: userReducer,
   detailuser: detailUserReducer,
   destination: destinationReducer,
-})
->>>>>>> master
+});
 
 export default rootReducers;
