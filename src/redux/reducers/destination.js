2641cbd48ef768630a236bdf02ebe3f9d7b3eb7f
hakamfaza <hakamfaza@gmail.com>
2022-05-09 01:36:37 +0700

    update type

--- src/redux/reducers/destination.js
+++ src/redux/reducers/destination.js
@@ -1,5 +1,5 @@
 /* eslint-disable default-param-last */
-import { GET_USER_PENDING, GET_USER_SUCCESS, GET_USER_FAILED } from '../actions/types';
+import { GET_DESTINATION_PENDING, GET_DESTINATION_SUCCESS, GET_DESTINATION_FAILED } from '../actions/types';
 
 const initialState = {
   isLoading: false,
@@ -10,16 +10,16 @@ const initialState = {
 
 const destinationReducer = (state = initialState, action) => {
   switch (action.type) {
-    case GET_USER_PENDING:
+    case GET_DESTINATION_PENDING:
       return { ...state, isLoading: true };
-    case GET_USER_SUCCESS:
+    case GET_DESTINATION_SUCCESS:
       return {
         ...state,
         isLoading: false,
         isError: false,
         data: action.payload,
       };
-    case GET_USER_FAILED:
+    case GET_DESTINATION_FAILED:
       return {
         ...state,
         isLoading: false,
