80227787238265ce72a3e0648909af9f2a60c14d
hanifudin0597 <hanifudin0597@gmail.com>
2022-05-11 01:40:11 +0700

    fixing profile

--- src/redux/actions/user.js
+++ src/redux/actions/user.js
@@ -66,4 +66,24 @@ export const updateUser = (body) => {
   })
 };
 
+export const updatePhoto = (body) => {
+  const token = localStorage.getItem("token")
+  const id = localStorage.getItem("id")
 
+  // console.log(body)
+
+  return new Promise((resolve, reject) => {
+    axios.put(`${process.env.REACT_APP_API_URL}/user/${id}/photo`, body, {
+      headers: {
+        token: token,
+        "Content-Type": "multipart/form-data"
+      }
+    })
+      .then((response) => {
+        resolve(response.data)
+      })
+      .catch((err) => {
+        reject(err)
+      })
+  })
+};
