d5d75505dde75d6a0b55549f7f1382da80a77e52
hakamfaza <hakamfaza@gmail.com>
2022-05-11 20:11:03 +0700

    update private route

--- src/router/index.js
+++ src/router/index.js
@@ -34,7 +34,11 @@ export default function router() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/search" element={<SearchFlight />} />
+        <Route path="/search" element={
+          <PrivateRoute>
+            <SearchFlight />
+          </PrivateRoute>
+        } />
         <Route
           path={"/ticket-detail/:id"}
           element={
